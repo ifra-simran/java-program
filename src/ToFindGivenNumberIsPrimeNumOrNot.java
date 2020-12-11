0edcd2914c1f73933e00ad7ed53631b89d91615a
ifra-simran <69076684+ifra-simran@users.noreply.github.com>
2020-12-11 23:17:27 +0530

    Looping concepts

---
+++ src/ToFindGivenNumberIsPrimeNumOrNot.java
@@ -0,0 +1,21 @@
+
+public class ToFindGivenNumberIsPrimeNumOrNot {
+	public static void main(String args[]) {
+		int num = 7;
+		boolean flag = false;
+		for (int i = 2; i <= num / 2; ++i) {
+			// condition for non -prime number
+			if (num % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+
+		if (!flag)
+			System.out.println(num + " is a prime number");
+		else
+			System.out.println(num + " is not a prime number");
+
+	}
+
+}
---
+++ src/ToFindSumOfNaturalNumbers.java
@@ -0,0 +1,11 @@
+
+public class ToFindSumOfNaturalNumbers {
+	public static void main(String args[]) {
+		System.out.println("Program to find sum of natural number between 5 to 25");
+		int i, sum = 0;
+		for (i = 5; i <= 25; i++)
+			sum = sum + i;
+		System.out.println("Sum of natural number between 5 to 25 is : " + sum);
+
+	}
+}
