39e3284d4191893a0f9856ac1516627021f89941
ifra-simran <69076684+ifra-simran@users.noreply.github.com>
2020-12-11 23:18:58 +0530

    Methods

---
+++ src/methods/ArthmaticSubtraction.java
@@ -0,0 +1,23 @@
+package methods;
+
+public class ArthmaticSubtraction {
+	
+
+
+		int sub(int a,int b) {
+			//System.out.println("the program start");
+			int sub=a-b;
+			return sub;
+		}
+		double sub(double l,double w) {
+			double sub= l - w;
+			return sub;
+		}
+	      void sub() 
+	      {
+		System.out.println("the program started");
+	}
+
+	}
+
+
