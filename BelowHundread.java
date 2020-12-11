d388836f8b021fdc2602832dad18c4b2c89759bc
ifra-simran <69076684+ifra-simran@users.noreply.github.com>
2020-12-11 23:23:49 +0530

    Programs

---
+++ BelowHundread.java
@@ -0,0 +1,15 @@
+class BelowHundread{
+   public static void main(String args[]){
+   
+   int a=54;
+   if(a<100){
+   System.out.println( a + "The given number is below 100");
+   }
+   else 
+	   
+   if(a>100){
+   System.out.println("ignore");
+   
+   }
+   }
+   }
---
+++ PositiveOrNegative.java
@@ -0,0 +1,23 @@
+class PositiveOrNegative{
+   public static void main(String args[]){
+   
+   int a=0;
+   if(a<0)
+   {
+   System.out.println(a+ " The given number is negative");
+   }
+   else 
+   
+	   if( a > 0){
+   System.out.println(a+"The given number is positive");
+      }
+	  else
+	  {
+		 System.out.println( a + " The given number is neither negative nor positive"); 
+	  }
+   
+    }
+   
+
+
+}
